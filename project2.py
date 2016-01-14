2eb3630f2c7b3b324597f7b432f5496f6c908ba5
bklo94 <brandonklo94@gmail.com>
2016-01-13 19:05:13 -0800

    first commit

--- project2.py
+++ project2.py
@@ -1,20 +1,30 @@
-#
-#
-#2847 is sum of all lower case ASCII
+#project2.py
+#Brandon Lo and Derek Tan, 4/30/15
 
 def isPangram(sentence):
     sentence = sentence.lower()
-    noPunc = ""
     length = len(sentence)
     pos = 0
     total = 0
+    order = []
     while pos < length:
         if ord(sentence[pos]) >= 97 and ord(sentence[pos]) <=122:
-            total = total + ord(sentence[pos])
+            order.append(sentence[pos])
             pos = pos + 1
         else:
             pos = pos + 1
-    if total == 2847:
+    start = 0
+    len(order)
+    for i in range(0, len(order)):
+        j = order[i]
+        for k in range(0, len(order)):
+            l = order.count(j)
+            if l > 1:
+                order[i] = '\x00'
+    for m in range(0, len(order)):
+        order[m] = ord(order[m])
+    n = sum(order)
+    if n == 2847:
         return('True')
     else:
         return('False')
@@ -43,3 +53,68 @@ def isPalindrome(sentence):
         return('True')
     else:
         return('False')
+
+def convert(text):
+    pos = 0
+    text2 = text.upper()
+    if text2 != text:
+        while pos < len(text):
+            first = text.lower()[pos]
+            if pos == 0:
+                if first == "a" or first == "e" or first == "i" or first == "o" or first == "u":
+                    a = text + "way"
+                    break
+                elif first == "q" and text.lower()[pos+1] == "u":
+                    a = text [pos+2:] + text[:pos+2] + "ay"
+                    break
+                else:
+                    pos = pos + 1
+            elif pos > 0:
+                if first == "a" or first == "e" or first == "i" or first == "o" or first == "u" or first == "y":
+                    a = text [pos:] + text[:pos] +"ay"
+                    break
+                elif first == "q" and text.lower()[pos+1] == "u":
+                    a = text [pos+2:] + text[:pos+2] + "ay"
+                    break
+                else:
+                    pos = pos + 1
+    if text2 == text:
+        while pos < len(text):
+            first = text.lower()[pos]
+            if pos == 0:
+                if first == "a" or first == "e" or first == "i" or first == "o" or first == "u":
+                    a = text + "WAY"
+                    break
+                elif first == "q" and text.lower()[pos+1] == "u":
+                    a = text [pos+2:] + text[:pos+2] + "AY"
+                    break
+                else:
+                    pos = pos + 1
+            elif pos > 0:
+                if first == "a" or first == "e" or first == "i" or first == "o" or first == "u" or first == "y":
+                    a = text [pos:] + text[:pos] +"AY"
+                    break
+                elif first == "q" and text.lower()[pos+1] == "u":
+                    a = text [pos+2:] + text[:pos+2] + "AY"
+                    break
+                else:
+                    pos = pos + 1
+    return (a)
+
+import re
+def pigLatin(text):
+    position = 0
+    length3 = len(text)
+    order = []
+    order = re.split('(\W+)', text)
+    position3 = 0
+    final = ""
+    length4 = len(order)        
+    while position3 < len(order):
+        if order[position3].isalpha():
+            final = final + convert(order[position3])
+        else:
+            final = final + order[position3]
+        position3 = position3 + 1
+    return(final)
+    
